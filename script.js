6fdcdae25fad324b608f52c23baa554b06c8aab2
Polaris2001 <nevillelin3@gmail.com>
2023-11-03 15:59:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-//Declare variables
+// Declare variables
 var energyCount = 10;
 var energyPerSec = 0;
 var dim1Count = 0;
 var dim1EnergyPerSec = 0;
 
-//Declare constants
-const numberdisplay = document.getElementById('numberDisplay');
+// Declare constants
+const numberDisplay = document.getElementById('numberDisplay');
 const dim1Display = document.getElementById('dim1Count');
 
 // Update energy displayed on the main panel every 100ms
 function updateEnergyCount() {
-    energyCount += energyPerSec/10;
-    numberdisplay.textContent = 'You have ' + energyCount + ' energy. (' + energyPerSec + '/s)';
+    energyCount += energyPerSec / 10;
+    numberDisplay.textContent = 'You have ' + energyCount + ' energy. (' + energyPerSec + '/s)';
 }
 
 // Update dimension 1 data when it is bought
-dim1Display.onclick = function updateDim1() {
+dim1Display.onclick = function() {
     dim1Count++;
     energyPerSec++;
     dim1Display.textContent = 'You have: ' + dim1Count;
-}
+};
 
-
-setInterval(updateEnergyCount(), 100);
+setInterval(updateEnergyCount, 100);
