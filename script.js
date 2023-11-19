b7b4484046c0ed5c459ba0cf71a29fc49293b7ff
Polaris2001 <nevillelin3@gmail.com>
2023-11-19 19:39:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-// Declare variables for energy display
-let energyCountJS = 0;
-let energyPerSecondJS = 0;
+ // Declare variables for energy display
+  let energyCountJS = 0;
+  let energyPerSecondJS = 0;
 
-// Update energy count in HTML
-function updateEnergyCount() {
-  document.getElementById("energyCount").textContent = "You have " + energyCountJS + " energy.";
-}
+  // Update energy count in HTML
+  function updateEnergyCount() {
+    document.getElementById("energyCount").textContent = "You have " + energyCountJS + " energy.";
+  }
 
-// Update energy per second in HTML
-function updateEnergyPerSecond() {
-  document.getElementById("energyPerSecond").textContent = "You generate " + energyPerSecondJS + " energy per second.";
-}
+  // Update energy per second in HTML
+  function updateEnergyPerSecond() {
+    document.getElementById("energyPerSecond").textContent = "You generate " + energyPerSecondJS + " energy every second.";
+  }
 
-// Manual generate energy
-function manualEnergy() {
-  energyCountJS++;
-  updateEnergyCount(); // Update the energy count in the HTML
-}
+  // Manual generate energy
+  function manualEnergy() {
+    energyCountJS++;
+    updateEnergyCount(); // Update the energy count in the HTML
+  }
 
-// Add event listener to the manual energy button
-document.getElementById("manualEnergy").addEventListener("click", manualEnergy);
+  // Add event listener to the manual energy button
+  document.getElementById("manualEnergy").addEventListener("click", manualEnergy);
 
-// Call the updateEnergyPerSecond function initially
-updateEnergyPerSecond();
+  // Call the updateEnergyPerSecond function initially
+  updateEnergyPerSecond();
 
-// Run the updateEnergyPerSecond function every second (1000ms)
-setInterval(updateEnergyPerSecond, 1000);
+  // Run the updateEnergyCount function every second (1000ms)
+  setInterval(function () {
+    energyCountJS += energyPerSecondJS;
+    updateEnergyCount();
+  }, 1000);
