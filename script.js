d0e7284b0e2cc6776ba67d52b2a892759b898f64
Polaris2001 <nevillelin3@gmail.com>
2023-11-22 18:20:44 +0800

    d

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+ // ------ Part 1 -- Declaring variables ------
  // Declare variables for energy display
   let energyCountJS = 0;
   let energyPerSecondJS = 0;
+ 
 
+  // ------ Part 2 -- General Functions ------
+  // Manual generate energy
+  function manualEnergy() {
+    energyCountJS++;
+  }
+
+  // Add event listener to the manual energy button
+  document.getElementById("manualEnergy").addEventListener("click", manualEnergy);
+
+
+  // ------ Part 3 -- Update Statistics ------
   // Update energy count in HTML
   function updateEnergyCount() {
     document.getElementById("energyCount").textContent = "You have " + energyCountJS + " energy.";
@@ -12,20 +25,8 @@
     document.getElementById("energyPerSecond").textContent = "You generate " + energyPerSecondJS + " energy every second.";
   }
 
-  // Manual generate energy
-  function manualEnergy() {
-    energyCountJS++;
-    updateEnergyCount(); // Update the energy count in the HTML
-  }
+  // ------ Part 4 -- Run functions ------
+  // Execute both function every 100ms
+  setInterval(updateEnergyCount, 100);
+  setInterval(updateEnergyPerSecond, 100);
 
-  // Add event listener to the manual energy button
-  document.getElementById("manualEnergy").addEventListener("click", manualEnergy);
-
-  // Call the updateEnergyPerSecond function initially
-  updateEnergyPerSecond();
-
-  // Run the updateEnergyCount function every second (1000ms)
-  setInterval(function () {
-    energyCountJS += energyPerSecondJS;
-    updateEnergyCount();
-  }, 1000);
